5e724ec1fd518656695b3b2d5d68b5eba70b503c
xiank he <875155591@qq.com>
2025-02-28 12:15:55 +0800

    distill-any-depth

--- scripts/00_infer.sh
+++ scripts/00_infer.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 GPU_ID=0
-model_list=('xxx')
+model_list=('xxx')  # 替换为你实际的模型列表
 
 for model in "${model_list[@]}"; do
-
     CUDA_VISIBLE_DEVICES=${GPU_ID} \
     python tools/testers/infer.py \
         --seed 1234 \
         --checkpoint 'checkpoint/large/model.safetensors' \
         --processing_res 0 \
         --output_dir output/${model} \
-        --arch_name 'depthanything' \
-    done
+        --arch_name 'depthanything'
+done
--- tools/testers/infer.py
+++ tools/testers/infer.py
@@ -1,6 +1,8 @@
 import argparse
 import logging
+import sys
 import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import os.path as osp
 import numpy as np
 import torch
